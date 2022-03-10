35bc8668c3c0f6f8bbf9ae2fa159ac072fed8874
Samuel Ndirangu <sndirangu7@gmail.com>
2022-03-10 11:44:51 +0300

    Added docker compose
    Added a docker compose file to automate setting up of the nginx, mongo and node containers
    
    Added an ansible playbook to automate installing and fire up new instances with docker and spin up containers

--- src/functions/heartBeat.ts
+++ src/functions/heartBeat.ts
@@ -3,7 +3,7 @@ import { StatusCodes } from "http-status-codes";
 
 const heartBeat = async (req: express.Request, res: express.Response) => {
     console.log('Heart beat:: Beat Beat...');
-    res.status(StatusCodes.OK).send('Server is up: Heart beat:: Beat Beat...');
+    res.status(StatusCodes.OK).send('Server is up: Heart beat:: Beat Beats...');
 }
 
 export default heartBeat;
